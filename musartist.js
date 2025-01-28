f56f6df746f85c56c208e83bf808a4ce2faa7b31
aakash77815 <aakashkash146@gmail.com>
2025-01-28 15:04:45 +0530

    musartist.js

---
+++ musartist.js
@@ -0,0 +1,21 @@
+let artists=["anirudh","rahman","harris","hiphop","gvp"];
+let favart=prompt("Type ur fav artist:");
+switch (artists&&favart){
+    case "anirudh":
+        console.log("That's one of my favorites, too.");
+        break;
+    case "rahman":
+        console.log("That's one of my favorites, too.");
+        break;
+    case "harris":
+        console.log("That's one of my favorites, too.");
+        break;
+    case "hiphop":
+        console.log("That's one of my favorites, too.");
+        break;
+    case "gvp":
+        console.log("That's one of my favorites, too.");
+        break;
+    default:
+        console.log("sorry not my favourite");
+}
